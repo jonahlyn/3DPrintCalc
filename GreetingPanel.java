45f1e6d930ca0587494d701d3df1adff847dbefd
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2017-07-27 07:37:46 -0600

    Updated greeting text.

--- GreetingPanel.java
+++ GreetingPanel.java
@@ -12,7 +12,7 @@ public class GreetingPanel extends JPanel {
     private JLabel greeting; // holds greeting text
 
     public GreetingPanel(){
-        greeting = new JLabel("Instructions: ...");
+        greeting = new JLabel("Calculate the cost of creating a 3D printed object using a desktop FDM printer.");
 
         //Add the text to this panel.
         add(greeting);
